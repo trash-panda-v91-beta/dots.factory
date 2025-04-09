5dc1a98edc41cd7793d2b4935a5b920a6af4722f
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-04-09 10:50:29 +0200

    (raycast) refactor scripts

--- homes/_modules/shell/aws/default.nix
+++ homes/_modules/shell/aws/default.nix
@@ -1,6 +1,12 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.modules.shell.aws;
+  scripts = "raycast/scripts";
 in
 {
   options.modules.shell.aws = {
@@ -27,6 +33,32 @@ in
             aa = "aws_okta_keyman";
           };
         };
+      };
+      xdg.configFile."${scripts}/authenticate-to-aws.sh" = {
+        text = ''
+          #!${pkgs.bash}/bin/bash
+
+          # @raycast.schemaVersion 1
+          # @raycast.title Authenticate to AWS
+          # @raycast.mode inline
+          # @raycast.packageName Raycast Scripts
+          # @raycast.icon 🔑
+          # @raycast.currentDirectoryPath ~
+          # @raycast.needsConfirmation false
+          # Documentation:
+          # @raycast.description Authenticate to AWS using Okta
+
+          echo "Authenticating ..."
+          output=$(${config.home.homeDirectory}/usr/local/bin/aws_okta_keyman 2>&1)
+          rc=$?
+
+          if [ $rc -eq 0 ]; then
+              echo "Authentication successful."
+          else
+              echo "Authentication failed."
+          fi
+        '';
+        executable = true;
       };
     })
   ];
