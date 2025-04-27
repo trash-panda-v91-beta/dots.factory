f100999507bc1efc6b283157f0462ded0cf5a46c
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-04-27 09:08:07 +0200

    Remove unused keybindings

--- homes/_modules/terminals/ghostty/default.nix
+++ homes/_modules/terminals/ghostty/default.nix
@@ -72,28 +72,11 @@ in
       # To delete from the current position to the start of the line
       keybind = super+backspace=esc:w
 
-      # Go to the N window in tmux in tmux it is Ctrl+b+number
-      keybind = super+one=text:\x02\x31
-      keybind = super+two=text:\x02\x32
-      keybind = super+three=text:\x02\x33
-      keybind = super+four=text:\x02\x34
-      keybind = super+five=text:\x02\x35
-      keybind = super+six=text:\x02\x36
-      keybind = super+seven=text:\x02\x37
-      keybind = super+eight=text:\x02\x38
-      keybind = super+nine=text:\x02\x39
-
       # Close tmux pane on Cmd+w in tmux it is Ctrl+b+x (it is using a custom keybind in tmux to kill the pane)
       keybind = super+w=text:\x02\x78
 
       # Switch a tmux session on Cmd+o in tmux it is Ctrl+b+w
       keybind = super+o=text:\x02\x77
-
-      # Rename a tmux window on Cmd+r in tmux it is Ctrl+b+,
-      keybind = super+r=text:\x02\x2c
-
-      # Go to the previous tmux window on Cmd+h in tmux it is Ctrl+b+p
-      keybind = super+h=text:\x02\x70
 
       # Go to the last tmux session on Cmd+l in tmux it is Ctrl+b+n
       keybind = super+l=text:\x02\x6e
@@ -112,9 +95,6 @@ in
 
       # Map super+i to alt+i
       keybind = super+i=text:\x1bi
-
-      # Map super+s to alt+s
-      keybind = super+s=text:\x1bs
 
       # Map super+t to alt+t
       keybind = super+t=text:\x1bt
