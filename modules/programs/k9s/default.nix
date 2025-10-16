458d77b07b984dc4a5f6a853cc16c446b094af25
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-10-16 13:16:19 +0200

    feat(k9s): add inspect-pvc plugin for PVC exploration
    - introduce inspect-pvc plugin to k9s for exploring persistent volume claims
    - define custom kubectl command to run busybox pod with mounted PVC
    - add shortcut, description, and plugin configuration for improved usability

--- modules/programs/k9s/default.nix
+++ modules/programs/k9s/default.nix
@@ -10,6 +10,49 @@ delib.module {
     {
       programs.k9s = {
         enable = cfg.enable;
+        plugins = {
+          inspect-pvc = {
+            shortCut = "s";
+            confirm = false;
+            description = "Explore PVC";
+            scopes = [ "persistentvolumeclaims" ];
+            command = "sh";
+            background = false;
+            args = [
+              "-c"
+              ''
+                kubectl run --rm -it k9s-inspecting-''${NAMESPACE}-''${NAME} --image=busybox --restart=Never --namespace ''${NAMESPACE} --context ''${CONTEXT} --overrides='{
+                    "spec": {
+                        "containers": [
+                            {
+                                "image": "busybox",
+                                "name": "k9s-inspecting-''${NAMESPACE}-''${NAME}",
+                                "volumeMounts": [
+                                    {
+                                        "mountPath": "/inspecting-pvc-''${NAMESPACE}-''${NAME}",
+                                        "name": "k9s-inspected-pvc"
+                                    }
+                                ],
+                                "workingDir": "/inspecting-pvc-''${NAMESPACE}-''${NAME}",
+                                "stdin": true,
+                                "tty": true,
+                                "imagePullPolicy": "Always"
+                            }
+                        ],
+                        "volumes": [
+                            {
+                                "name": "k9s-inspected-pvc",
+                                "persistentVolumeClaim": {
+                                    "claimName": "''${NAME}"
+                                }
+                            }
+                        ]
+                    }
+                }' --command -- sh
+              ''
+            ];
+          };
+        };
       };
     };
 }
