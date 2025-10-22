b10fa0f6d92416db794869805b102cc5a4482f12
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-10-22 13:03:31 +0200

    feat(darwin): add cmb host configuration
    - add default.nix for cmb host with coding features and user config
    - add hardware.nix for cmb host with platform and state version settings

---
+++ hosts/darwin/cmb/default.nix
@@ -0,0 +1,30 @@
+{
+  delib,
+  ...
+}:
+delib.host {
+  name = "cmb";
+
+  features = [
+    "coding"
+    "githubCopilot"
+    "python"
+  ];
+  rice = "cyberdream-dark";
+  type = "desktop";
+
+  homeManagerUser = "CORPORATE_USER";
+
+  myconfig =
+    {
+      myconfig,
+      ...
+    }:
+    {
+      user.name = "CORPORATE_USER";
+      programs = {
+        git.userEmail = myconfig.user.email;
+        nushell.enable = true;
+      };
+    };
+}
