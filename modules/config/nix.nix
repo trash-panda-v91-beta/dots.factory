8fbda02f2554a3e28b83824705f8cf196a1fd7e8
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-11-12 20:35:27 +0100

    feat(nix): split nix config for system and home-manager
    - Separate system-level and home-manager nix settings
    - Add Cachix binary caches and trusted-users to system config
    - Refactor module to use new systemSettings and homeSettings

--- modules/config/nix.nix
+++ modules/config/nix.nix
@@ -5,7 +5,38 @@
   ...
 }:
 let
-  shared.nix = {
+  systemSettings =
+    { myconfig, ... }:
+    {
+      nix = {
+        package = lib.mkForce pkgs.nixVersions.latest;
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          warn-dirty = false;
+
+          substituters = [
+            "https://cache.nixos.org"
+            "https://nix-community.cachix.org"
+            "https://nixpkgs-unfree.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+          ];
+
+          trusted-users = [
+            "root"
+            myconfig.user.name
+          ];
+        };
+      };
+    };
+
+  homeSettings.nix = {
     package = lib.mkForce pkgs.nixVersions.latest;
     settings = {
       experimental-features = [
@@ -19,6 +50,7 @@ in
 delib.module {
   name = "nix";
 
-  nixos.always = shared;
-  home.always = shared;
+  darwin.always = systemSettings;
+  nixos.always = systemSettings;
+  home.always = homeSettings;
 }
