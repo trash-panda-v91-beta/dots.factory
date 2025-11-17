c2f9531b1b8f64974a3bb52998cb906ecdde1988
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-11-17 22:56:50 +0100

    feat(raycast): enable and update sync scripts
    - Enable open-notifications.js and update-dots-factory.sh in config
    - Rewrite update-dots-factory.sh to use nh and GitHub token

--- modules/programs/raycast/default.nix
+++ modules/programs/raycast/default.nix
@@ -12,14 +12,35 @@ delib.module {
   myconfig.ifEnabled.unfreePackages.allow = [ "raycast" ];
 
   home.ifEnabled =
+    {
+      myconfig,
+      ...
+    }:
     let
       scriptsFolder = "raycast/scripts";
     in
     {
       xdg.configFile = {
         "${scriptsFolder}/dismiss-notifications.js".source = scripts/dismiss-notifications.js;
-        # "open-notifications.js".source = scripts/open-notifications.js;
-        # "update-dots-factory.sh".source = scripts/update-dots-factory.sh;
+        "${scriptsFolder}/open-notifications.js".source = scripts/open-notifications.js;
+        "${scriptsFolder}/update-dots-factory.sh".text = ''
+          #!/bin/bash
+
+          # @raycast.schemaVersion 1
+          # @raycast.title Sync dots
+          # @raycast.mode compact
+          # @raycast.packageName Raycast Scripts
+          # @raycast.icon ⚫
+          # @raycast.currentDirectoryPath ~
+          # @raycast.needsConfirmation false
+          # Documentation:
+          # @raycast.description Run NixOS rebuild command
+
+          GH_TOKEN=$(${pkgs.lib.getExe pkgs._1password-cli} read "op://Private/dots.vault Read Access/password")
+          echo "Building and switching to configuration for '${myconfig.host.name}' ..."
+          NIX_CONFIG="access-tokens = github.com=$GH_TOKEN" nh darwin switch . --hostname {{hostname}}
+        '';
+
       };
       home.packages = [ pkgs.raycast ];
     };
