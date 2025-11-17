7f46e71bb46c49058ab2f6ce0296c8c091fda0b5
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-11-17 15:06:02 +0100

    feat(attic-client): add attic-client module and cache config
    - Add new attic-client program module with enable option
    - Include attic-client in home packages when enabled
    - Register nebular-grid attic cache and trusted key in nix config

--- modules/config/nix.nix
+++ modules/config/nix.nix
@@ -18,12 +18,14 @@ let
           warn-dirty = false;
 
           substituters = [
+            "https://attic.nebular-grid.spacedots"
             "https://cache.nixos.org"
             "https://nix-community.cachix.org"
             "https://nixpkgs-unfree.cachix.org"
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "dots:Vn4tegfbCNMsaGnaA/GKG2gwZrWNxn6oUXD2kkbAUcU="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
           ];
