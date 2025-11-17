b9484890604ec9cc716f588f7aa035eb495369f4
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-11-17 16:43:22 +0100

    refactor(ci): update workflows and nix cache configuration
    - Remove unused Cachix steps from most GitHub workflows
    - Add validate job and improve build matrix in CI
    - Update substituters and trusted-public-keys in nix config
    - Set attic-client module to disabled by default
    - Ensure NIX_CONFIG is set for secure token usage in jobs

--- modules/config/nix.nix
+++ modules/config/nix.nix
@@ -18,16 +18,14 @@ let
           warn-dirty = false;
 
           substituters = [
-            "https://attic.nebular-grid.spacedots"
-            "https://cache.nixos.org"
-            "https://nix-community.cachix.org"
-            "https://nixpkgs-unfree.cachix.org"
+            "https://dots-factory.cachix.org?priority=1"
+            "https://cache.nixos.org?priority=10"
+            "https://nix-community.cachix.org?priority=20"
           ];
           trusted-public-keys = [
+            "dots-factory.cachix.org-1:x8uhqhYtk4U697Ql3+mRI3adtVkvNRRr5XbyzXv9YSU="
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-            "dots:Vn4tegfbCNMsaGnaA/GKG2gwZrWNxn6oUXD2kkbAUcU="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
           ];
 
           trusted-users = [
