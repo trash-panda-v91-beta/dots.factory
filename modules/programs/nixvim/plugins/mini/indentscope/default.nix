c40789e07c8de371b76249e14028db7cad9e8c46
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-11-21 20:55:02 +0100

    feat(nixvim): add snacks.scope plugin and update mini options
    - Add new snacks.scope plugin module with semantic scope detection
    - Change default enable option for mini.extra and mini.indentscope to false
    - Configure treesitter blocks for improved scope handling in snacks.scope

--- modules/programs/nixvim/plugins/mini/indentscope/default.nix
+++ modules/programs/nixvim/plugins/mini/indentscope/default.nix
@@ -1,7 +1,7 @@
 { delib, ... }:
 delib.module {
   name = "programs.nixvim.plugins.mini.indentscope";
-  options = delib.singleEnableOption true;
+  options = delib.singleEnableOption false;
   home.ifEnabled.programs.nixvim = {
     plugins.mini = {
       enable = true;
