cdc4a39568bdf4d51079a7061be6c3aead0e3235
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-12-12 10:31:47 +0100

    feat(nixvim): enhance lint plugin with custom autocmd and actionlint args
    - Change lazyLoad event to BufReadPost for earlier lint loading
    - Add custom autocmd to trigger linting on buffer events with context checks

--- modules/programs/nixvim/plugins/lint/default.nix
+++ modules/programs/nixvim/plugins/lint/default.nix
@@ -8,7 +8,7 @@ delib.module {
     plugins.lint = {
       enable = true;
       lazyLoad.settings = {
-        event = [ "DeferredUIEnter" ];
+        event = [ "BufReadPost" ];
       };
 
       lintersByFt = {
@@ -26,11 +26,71 @@ delib.module {
       };
 
       linters = {
-        actionlint.cmd = pkgs.lib.getExe pkgs.actionlint;
+        actionlint = {
+          cmd = pkgs.lib.getExe pkgs.actionlint;
+          # TODO: remove after https://github.com/mfussenegger/nvim-lint/pull/887 is merged
+          args = [
+            "-format"
+            "{{json .}}"
+            "-stdin-filename"
+            {
+              __raw = ''
+                function()
+                  return vim.api.nvim_buf_get_name(0)
+                end
+              '';
+            }
+            "-"
+          ];
+        };
         cfn_lint.cmd = pkgs.lib.getExe pkgs.python3Packages.cfn-lint;
         hadolint.cmd = pkgs.lib.getExe pkgs.hadolint;
         yamllint.cmd = pkgs.lib.getExe pkgs.yamllint;
         zizmor.cmd = pkgs.lib.getExe pkgs.zizmor;
+      };
+
+      autoCmd = {
+        event = [
+          "BufEnter"
+          "BufWritePost"
+          "InsertLeave"
+        ];
+        callback = {
+          __raw = ''
+            function()
+              -- Try to load the lint module, return early if not loaded yet
+              local ok, lint = pcall(require, "lint")
+              if not ok then
+                return
+              end
+
+              local filetype = vim.bo.filetype
+              
+              -- Skip linting for special buffer types
+              if vim.bo.buftype ~= "" then
+                return
+              end
+              
+              -- Skip linting for non-lintable filetypes
+              local skip_filetypes = { "gitcommit", "gitrebase", "help", "qf" }
+              if vim.tbl_contains(skip_filetypes, filetype) then
+                return
+              end
+
+              -- For GitHub Actions workflows, run from repository root
+              -- so actionlint can find .github/actionlint.yaml config
+              if filetype == "yaml.ghaction" then
+                local repo_root = vim.fs.root(0, ".git")
+                if repo_root then
+                  lint.try_lint(nil, { cwd = repo_root })
+                  return
+                end
+              end
+
+              lint.try_lint()
+            end
+          '';
+        };
       };
     };
 
