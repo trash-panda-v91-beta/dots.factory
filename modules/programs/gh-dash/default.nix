312738bed772660a1864db3c3895d2dc147acd37
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-12-17 15:00:59 +0100

    refactor(programs/gh-dash): simplify settings configuration
    - Remove unnecessary attrsOption for settings definition

--- modules/programs/gh-dash/default.nix
+++ modules/programs/gh-dash/default.nix
@@ -8,7 +8,6 @@ delib.module {
 
   options.programs.gh-dash = with delib; {
     enable = boolOption host.codingFeatured;
-    settings = attrsOption { };
   };
 
   home.ifEnabled =
@@ -16,42 +15,40 @@ delib.module {
     {
       programs.gh-dash = {
         enable = cfg.enable;
-        settings = delib.lib.recursiveUpdate
-          {
-            prSections = [
-              {
-                title = "My Pull Requests";
-                filters = "is:open author:@me";
-              }
-              {
-                title = "Needs My Review";
-                filters = "is:open review-requested:@me";
-              }
-              {
-                title = "Involved";
-                filters = "is:open involves:@me -author:@me";
-              }
-            ];
-            issuesSections = [
-              {
-                title = "My Issues";
-                filters = "is:open author:@me";
-              }
-              {
-                title = "Assigned";
-                filters = "is:open assignee:@me";
-              }
-            ];
-            defaults = {
-              preview = {
-                open = true;
-                width = 50;
-              };
-              prsLimit = 20;
-              issuesLimit = 20;
+        settings = {
+          prSections = [
+            {
+              title = "My Pull Requests";
+              filters = "is:open author:@me";
+            }
+            {
+              title = "Needs My Review";
+              filters = "is:open review-requested:@me";
+            }
+            {
+              title = "Involved";
+              filters = "is:open involves:@me -author:@me";
+            }
+          ];
+          issuesSections = [
+            {
+              title = "My Issues";
+              filters = "is:open author:@me";
+            }
+            {
+              title = "Assigned";
+              filters = "is:open assignee:@me";
+            }
+          ];
+          defaults = {
+            preview = {
+              open = true;
+              width = 50;
             };
-          }
-          cfg.settings;
+            prsLimit = 20;
+            issuesLimit = 20;
+          };
+        };
       };
     };
 }
