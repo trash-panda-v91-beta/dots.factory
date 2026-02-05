5bf81750a7c8c9f82692acfecb814b0b730118bc
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-02-05 06:11:47 +0100

    feat(nixvim/octo): add pr update-branch command with rebase

--- modules/programs/nixvim/plugins/octo/default.nix
+++ modules/programs/nixvim/plugins/octo/default.nix
@@ -60,6 +60,33 @@ delib.module {
                     end
                   end
                 '';
+                update.__raw = ''
+                  function()
+                    local gh = require("octo.gh")
+                    local picker = require("octo.picker")
+                    local utils = require("octo.utils")
+
+                    local buffer = utils.get_current_buffer()
+
+                    local update_branch = function(number)
+                      local cb = function()
+                        utils.info("PR branch updated with rebase")
+                      end
+                      local opts = { cb = cb }
+                      gh.pr.update_branch({ number, rebase = true, opts = opts })
+                    end
+
+                    if not buffer or not buffer:isPullRequest() then
+                      picker.prs({
+                        cb = function(selected)
+                          update_branch(selected.obj.number)
+                        end,
+                      })
+                    elseif buffer:isPullRequest() then
+                      update_branch(buffer.node.number)
+                    end
+                  end
+                '';
               };
             };
           };
@@ -99,6 +126,7 @@ delib.module {
                   { localleader .. 'p', group = 'Pull Request', buffer = event.buf },
                   { localleader .. 'r', group = 'React', buffer = event.buf },
                   { localleader .. 'v', group = 'Review', buffer = event.buf },
+                  { localleader .. 'y', group = 'Yank/Copy', buffer = event.buf },
                   
                   -- Hide unwanted global groups/keymaps that conflict with octo keymaps
                   -- Hide LSP group (conflicts with Label)
@@ -125,6 +153,49 @@ delib.module {
                 vim.keymap.set('n', localleader .. 'pa', '<cmd>Octo pr auto<cr>', {
                   buffer = event.buf,
                   desc = 'Auto-merge PR'
+                })
+                
+                -- Update branch with rebase keymap
+                vim.keymap.set('n', localleader .. 'pu', '<cmd>Octo pr update<cr>', {
+                  buffer = event.buf,
+                  desc = 'Update branch (rebase)'
+                })
+                
+                -- Open in browser keymap (mirrors <C-b>)
+                vim.keymap.set('n', localleader .. 'b', function()
+                  local utils = require('octo.utils')
+                  local buffer = utils.get_current_buffer()
+                  if buffer then
+                    utils.open_in_browser(buffer)
+                  end
+                end, {
+                  buffer = event.buf,
+                  desc = 'Open in browser'
+                })
+                
+                -- Copy URL keymap (mirrors <C-y>)
+                vim.keymap.set('n', localleader .. 'yu', function()
+                  local utils = require('octo.utils')
+                  local buffer = utils.get_current_buffer()
+                  if buffer then
+                    utils.copy_url(buffer)
+                  end
+                end, {
+                  buffer = event.buf,
+                  desc = 'Copy URL'
+                })
+                
+                -- Copy SHA keymap (mirrors <C-e>)
+                vim.keymap.set('n', localleader .. 'ys', function()
+                  local utils = require('octo.utils')
+                  local buffer = utils.get_current_buffer()
+                  if buffer and buffer.node and buffer.node.headRefOid then
+                    vim.fn.setreg('+', buffer.node.headRefOid)
+                    utils.info('Copied SHA: ' .. buffer.node.headRefOid)
+                  end
+                end, {
+                  buffer = event.buf,
+                  desc = 'Copy SHA'
                 })
                 
                 -- Add extra keymaps from configuration
