0feab3098aa25a7f17e474a60583464f93c893fd
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-03-31 09:33:34 +0200

    refactor(yazi): simplify module, remove custom plugins and packages

--- modules/programs/yazi/default.nix
+++ modules/programs/yazi/default.nix
@@ -1,63 +1,21 @@
 {
   delib,
-  pkgs,
-  lib,
   ...
 }:
 delib.module {
   name = "programs.yazi";
 
   options.programs.yazi = with delib; {
-    enable = boolOption true;
-
-    plugins = attrsOption { };
-    extraPackages = listOfOption package [ ];
-    extraInitLua = strOption "";
+    enable = boolOption false;
   };
 
-  home.ifEnabled =
-    { cfg, ... }:
-    let
-      # Read Lua from separate file and append user customizations
-      initLua = builtins.readFile ./init.lua + cfg.extraInitLua;
-    in
-    {
-      programs.yazi = {
-        enable = true;
-
-        # Enable shell integrations
-        enableBashIntegration = true;
-        enableFishIntegration = true;
-        enableNushellIntegration = true;
-        enableZshIntegration = true;
-
-        package = pkgs.yazi.override {
-          extraPackages = cfg.extraPackages ++ [
-            pkgs.jq
-            pkgs._7zz
-            pkgs.fd
-            pkgs.ripgrep
-            pkgs.fzf
-            pkgs.zoxide
-          ];
-        };
-
-        # Plugins - configured on Linux only
-        plugins =
-          cfg.plugins
-          // (lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-            inherit (pkgs.yaziPlugins)
-              chmod
-              full-border
-              git
-              jump-to-char
-              smart-enter
-              smart-filter
-              ;
-          });
-
-        # Use separate Lua file
-        inherit initLua;
-      };
+  home.ifEnabled = {
+    programs.yazi = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableNushellIntegration = true;
+      enableZshIntegration = true;
     };
+  };
 }
