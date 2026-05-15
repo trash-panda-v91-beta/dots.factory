cbccb221c273ffa53bbf8cd3e152ae1ab26cdd2e
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-05-15 10:47:22 +0200

    feat(mise): add fnox-env plugin and environment config

--- modules/aspects/dev-tools.nix
+++ modules/aspects/dev-tools.nix
@@ -17,9 +17,19 @@
           programs.mise = {
             enable = true;
             enableNushellIntegration = true;
-            globalConfig.settings = {
-              env_cache = true;
-              env_cache_ttl = "8h";
+            globalConfig = {
+              env = {
+                "_.fnox-env" = {
+                  tools = true;
+                };
+              };
+              plugins = {
+                fnox-env = "https://github.com/jdx/mise-env-fnox";
+              };
+              settings = {
+                env_cache = true;
+                env_cache_ttl = "8h";
+              };
             };
           };
 
