2e5fb6308718e42bcba0e04c40be164063459209
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-05-15 10:47:16 +0200

    fix(yazi): adopt new shellWrapperName "y" default

--- modules/aspects/terminal.nix
+++ modules/aspects/terminal.nix
@@ -291,6 +291,7 @@
 
         programs.yazi = {
           enable = true;
+          shellWrapperName = "y";
           enableBashIntegration = true;
           enableFishIntegration = true;
           enableNushellIntegration = true;
