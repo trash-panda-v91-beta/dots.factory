2d8ace0335dc7f351e6b9d8c8d6acc7b0193cd94
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-08-16 22:06:19 +0200

    refactor(deps): track obsidian plugins+theme via npins (#56)

--- packages/obsidian-tasknotes-plugin/default.nix
+++ packages/obsidian-tasknotes-plugin/default.nix
@@ -1,26 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 let
-  version = "4.12.0";
-  base = "https://github.com/callumalpass/tasknotes/releases/download/${version}";
+  fetch =
+    name: pin:
+    pkgs.fetchurl {
+      inherit name;
+      url = pin.url;
+      hash = pin.hash;
+    };
+  manifest = fetch "manifest.json" inputs.obsidian-tasknotes-manifest;
+  version = (builtins.fromJSON (builtins.readFile manifest)).version;
 in
 pkgs.runCommandLocal "tasknotes-${version}" { } ''
   mkdir -p $out
-  cp ${
-    pkgs.fetchurl {
-      url = "${base}/main.js";
-      hash = "sha256-LjK1YLqhzgkS3738KU3uVeNZb2zpOGHnUpVybNqQW/4=";
-    }
-  } $out/main.js
-  cp ${
-    pkgs.fetchurl {
-      url = "${base}/manifest.json";
-      hash = "sha256-tcKwqHy2kgZaRiVdvMbbf/48l0JKzGeRpm7OVN5sJ4M=";
-    }
-  } $out/manifest.json
-  cp ${
-    pkgs.fetchurl {
-      url = "${base}/styles.css";
-      hash = "sha256-MAEomdWrh860I/twcKgkBvGzpMQ+goG2k14xHTU+HaU=";
-    }
-  } $out/styles.css
+  cp ${fetch "main.js"      inputs.obsidian-tasknotes-main}     $out/main.js
+  cp ${manifest}                                                 $out/manifest.json
+  cp ${fetch "styles.css"   inputs.obsidian-tasknotes-styles}   $out/styles.css
 ''
