45cf2b2b294f097ca8726c4f82945f627e603424
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-08-16 22:06:19 +0200

    refactor(deps): track obsidian plugins+theme via npins (#56)

--- packages/obsidian-minimal-theme/default.nix
+++ packages/obsidian-minimal-theme/default.nix
@@ -1,7 +1,2 @@
-{ pkgs, ... }:
-pkgs.fetchFromGitHub {
-  owner = "kepano";
-  repo = "obsidian-minimal";
-  rev = "9.0.2";
-  hash = "sha256-9ASmzalN8xcwQHi8uarVZ9IHXlb6QLn7AE2jUcAvdyM=";
-}
+{ inputs, ... }:
+inputs.obsidian-minimal-theme
