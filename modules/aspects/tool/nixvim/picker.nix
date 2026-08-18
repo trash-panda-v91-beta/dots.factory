ee15b38fe87c39894d693a59f65f647ba2a2e3dc
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-08-18 09:39:55 +0200

    fix(nixvim/picker): drop custom window config from mini.pick (#84)

--- modules/aspects/tool/nixvim/picker.nix
+++ modules/aspects/tool/nixvim/picker.nix
@@ -9,26 +9,7 @@
 
         plugins.mini-pick = {
           enable = true;
-          settings = {
-            options.use_cache = true;
-            window = {
-              config.__raw = ''
-                function()
-                  local height = math.floor(0.618 * vim.o.lines)
-                  local width = math.floor(0.618 * vim.o.columns)
-                  return {
-                    anchor = 'NW',
-                    height = height,
-                    width = width,
-                    row = math.floor(0.5 * (vim.o.lines - height)),
-                    col = math.floor(0.5 * (vim.o.columns - width)),
-                    border = 'single',
-                  }
-                end
-              '';
-              prompt_prefix = " ";
-            };
-          };
+          settings.options.use_cache = true;
         };
 
         plugins.mini-extra.enable = true;
