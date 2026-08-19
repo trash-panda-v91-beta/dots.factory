1eb070b4e9e491cd1ff53e3ac0645627c9175149
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2026-08-19 13:47:14 +0200

    chore(vicinae/aws-ext): add TODO to patch-s3.py

--- packages/aws-ext/patch-s3.py
+++ packages/aws-ext/patch-s3.py
@@ -1,3 +1,5 @@
+# TODO: remove once upstream fixes the bare {" "} text node in s3.tsx
+# https://github.com/raycast/extensions/blob/main/extensions/amazon-aws/src/s3.tsx
 import pathlib
 p = pathlib.Path("src/s3.tsx")
 p.write_text(p.read_text().replace('{" "}', ""))
